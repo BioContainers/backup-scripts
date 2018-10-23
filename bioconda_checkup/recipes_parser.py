32713eb71cf65cfce5f8115bb217fbdbca8cf676
raynooo <renaud.jullien@gmail.com>
2018-10-23 15:54:52 +0000

    Massive refactoring into a single script, not using intermediary files anymore

--- bioconda_checkup/recipes_parser.py
+++ bioconda_checkup/recipes_parser.py
@@ -8,6 +8,73 @@ import argparse
 import wget
 import sys
 import os
+import requests
+import re
+
+##################################################################################################
+####################Importing (probably) out-of-date tools list (BioContainers####################
+##################################################################################################
+
+#There are a couple of things to remove from the version number so it can be compared with what's stored$
+#Examples : XXX1-1-deb XXXlgpl-5-deb XXX9-9-deb-py2 XXXdfsg-1-deb
+def clean_version_number (original_version):
+        #myRegex = "(dfsg|lgpl).*$"
+        myRegex = "((ds[\d]*|dfsg[\d]*|lgpl)?(-[^-]+)*-deb.*$)|(-SNAPSHOT$)"
+        #Testing regex:
+        #matchRes = re.search(myRegex, original_version)
+        #if matchRes is not None:
+        #       print (matchRes.group())
+        original_version = re.sub(myRegex, '', original_version)
+
+        return original_version
+
+def import_out_of_date_list (url):
+	print ("Importing (supposedly) outdated tools from "+url)
+	biocont_tools = defaultdict(list)
+	
+	r = requests.get(url)
+	#print (r.status_code)
+	tree=r.json()['tree']
+	for crt in tree:
+		if (crt['path'].endswith("/Dockerfile")):
+			#print (crt['path'])
+			parts = crt['path'].split("/")
+			tool=parts[0]
+			version=parts[1]
+			##Cleaning version number of superfluous parts (ie: -deb*, dfsg*, lgpl*)
+			version=clean_version_number(version)
+			#print (version)
+			biocont_tools[tool].append(version)
+	return biocont_tools
+
+########################################################################################
+####################Importing (theoretically) newer tools (BioConda)####################
+########################################################################################
+
+def import_newer_list (url):
+	print ("Importing (theoretically) newer tools from "+url)
+	##Downloading the recipes
+	in_file=wget.download (url)
+	##Opening it
+	html_doc = open (in_file,"r")
+	
+	##Getting all BioConda's recipes
+	#Parsing the html doc
+	recipes = BeautifulSoup(html_doc, features="html.parser")
+	#Initialising the list to contain dicts by default
+	bioconda_recipes = defaultdict(list)
+	for row in recipes.find_all('tr'):
+		crtTool=row.get_text(" ").split(" ")
+		crtSoft=crtTool[0]
+		crtVersion=crtTool[1]
+		bioconda_recipes[crtSoft].append(crtVersion)
+	html_doc.close()
+	os.remove(in_file)
+	return bioconda_recipes
+
+##############################################################
+####################Comparing the two sets####################
+##############################################################
 
 ######################
 ###Sorting versions###
@@ -39,54 +106,21 @@ def is_reference_outdated (softwareName, refVersion, targetName, targetList, out
 	#else:
 		#print(softwareName+" is up-to-date compared to "+targetName, file=output)
 
-#################
-###Main method###
-#################
+###################################################
+####################Main method####################
+###################################################
 
 parser = argparse.ArgumentParser(description="Tests whether a given set of softwares have more up-to-date recipes in BioConda")
-parser.add_argument('software', help="Either a single software name or a file containing software names and versions")
-parser.add_argument('-v','--versions', nargs='*', help="If testing a single software, a series of versions separated by blank spaces")
-parser.add_argument('-f','--use-file', action="store_true", help="If used, the script will expect a file containing one software name and one or more version numbers per line as input")
-parser.add_argument('-u','--url',default="https://bioconda.github.io/recipes.html", help="The url where the html table to parse can be found")
+parser.add_argument('-u','--uptodate', default="https://bioconda.github.io/recipes.html", help="The url where the html table to parse can be found")
+parser.add_argument('-t','--outdated', default="https://api.github.com/repos/BioContainers/containers/git/trees/b21f5cdff0d9fcc9ff54d23f8be058b9fb5f45b6?recursive=1", help="The url to download the list of paths containing Dockerfiles")
 parser.add_argument('-o','--output', help="Output file where the results are stored, defaults to stdout")
 args = parser.parse_args()
 
-##Downloading the recipes
-in_file=wget.download (url=args.url)
+###Getting BioContainers tools
+biocont_tools = import_out_of_date_list(url=args.outdated)
 
-##Opening it
-html_doc = open (in_file,"r")
-
-##Getting all BioConda's recipes
-#Parsing the html doc
-recipes = BeautifulSoup(html_doc, features="html.parser")
-#Initialising the list to contain dicts by default
-bioconda_recipes = defaultdict(list)
-for row in recipes.find_all('tr'):
-	crtTool=row.get_text(" ").split(" ")
-	crtSoft=crtTool[0]
-	crtVersion=crtTool[1]
-	bioconda_recipes[crtSoft].append(crtVersion)
-html_doc.close()
-
-##Getting software names to compare
-ref_soft = defaultdict(list)
-if (args.use_file):
-	print ("Using a file as reference")
-	ref_file = open( args.software, "r")
-	line=ref_file.readline()
-	while line:
-		parts=line.rstrip().split(" ")
-		crt_soft=parts[0]
-		#print(crt_soft)
-		crt_version=parts[1:]
-		#print (crt_version)
-		ref_soft[crt_soft]=crt_version
-		line=ref_file.readline()
-	ref_file.close()
-else:
-	print ("Using input as reference")
-	ref_soft[args.software]=args.v
+##Getting BioConda tools
+bioconda_tools = import_newer_list(url=args.uptodate)
 
 ##And now to compare reference and BioConda
 if args.output is not None:
@@ -94,19 +128,14 @@ if args.output is not None:
 else:
 	output = sys.stdout
 
-for crt_ref in ref_soft.keys():
-	crt_ref_versions=order_version_list(ref_soft[crt_ref])
+for crt_biocont_tool in biocont_tools.keys():
+	crt_biocont_versions=order_version_list(biocont_tools[crt_biocont_tool])
 	#Let's find the best suitable tool in BioConda
-	best_match = difflib.get_close_matches(crt_ref, bioconda_recipes.keys(), n=1, cutoff=0.8)
+	best_match = difflib.get_close_matches(crt_biocont_tool, bioconda_tools.keys(), n=1, cutoff=0.8)
 	if len(best_match)!=0:
-		print ("--->"+crt_ref+"<--- was found to be similar to --->"+best_match[0]+"<---")
-		is_reference_outdated (crt_ref, crt_ref_versions[-1], best_match[0], bioconda_recipes[best_match[0]], output)
+		print ("\t--->"+crt_biocont_tool+"<--- was found to be similar to --->"+best_match[0]+"<---")
+		is_reference_outdated (crt_biocont_tool, crt_biocont_versions[-1], best_match[0], bioconda_tools[best_match[0]], output)
 
 
 if output is not sys.stdout:
 	output.close()
-
-
-##Cleanup
-#The downloaded file needs to be removed
-os.remove(in_file)
