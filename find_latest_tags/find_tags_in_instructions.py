1a1f3df41cfa283308247099424b2712c3377ad4
raynooo <renaud.jullien@gmail.com>
2018-12-10 17:50:06 +0000

    Made script more generic so it can find more varied stuff in a set of recipes since more issues with this kind of needs appeared

--- find_latest_tags/find_tags_in_instructions.py
+++ find_latest_tags/find_tags_in_instructions.py
@@ -5,16 +5,23 @@ import argparse
 import sys
 import os
 
-def check_this_recipe (path, tag, output):
+def check_this_recipe (path, tag, instruction, output):
 	with open(path, 'r') as content_file:
 		content = content_file.read()
 	dfp = DockerfileParser()
 	dfp.content = content
 	#print (dfp.labels['software.version'])
-	if 'base_image' in dfp.labels.keys() :
-		base_image = dfp.labels['base_image']
-		if base_image.endswith(tag):
-			print (path +"\t--->\t"+base_image, file=output)
+	#if 'base_image' in dfp.labels.keys() :
+	#	base_image = dfp.labels['base_image']
+	#	if base_image.endswith(tag):
+	#		print (path +"\t--->\t"+base_image, file=output)
+	#		return True
+	for crt_inst in dfp.structure:
+		if not crt_inst['instruction']==instruction:
+			continue
+		if tag in crt_inst['value']:
+			#print (crt_inst['value'])
+			print (path +"\t--->\t found "+tag+" in "+instruction, file=output)
 			return True
 	return False
 
@@ -26,6 +33,7 @@ parser = argparse.ArgumentParser(description="Tests whether Dockerfiles in your 
 parser.add_argument('-r','--rootdir', help="Path from which to search recursively", required="true")
 parser.add_argument('-t','--tag', help="Tag to look for", default="latest")
 parser.add_argument('-o','--output', help="Output file where the results are stored, defaults to stdout")
+parser.add_argument('-i','--instruction', help="Instruction in which to look for TAG", default="LABEL")
 args = parser.parse_args()
 
 if args.output is not None:
@@ -38,7 +46,7 @@ for root, dirs, files in os.walk (args.rootdir):
 	for file in files:
 		if file.endswith("Dockerfile"):
 			#print (os.path.join(root,file))
-			if check_this_recipe(os.path.join(root,file), args.tag, output):
+			if check_this_recipe(os.path.join(root,file), args.tag, args.instruction, output):
 				ctr+=1
 
 if output is not sys.stdout:
