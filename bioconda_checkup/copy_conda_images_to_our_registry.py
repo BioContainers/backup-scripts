c958f9571f94be7e689c24a405c8ba76d4e4b660
raynooo <renaud.jullien@gmail.com>
2019-01-24 16:58:55 +0000

    Test version: this should pull'n'push the first 5 missing tools from bioconda IF run from the appropriate machine

--- bioconda_checkup/copy_conda_images_to_our_registry.py
+++ bioconda_checkup/copy_conda_images_to_our_registry.py
@@ -22,6 +22,9 @@ def get_tags_from_response (response):
 	return res
 
 def pull_retag_and_push_images (client, tool_name, tags):
+	if tags==None:
+		print ("Nothing to do here")
+		return False
 	images = list()
 
 	##1) Pulling images
@@ -37,8 +40,9 @@ def pull_retag_and_push_images (client, tool_name, tags):
 			#return False
 
 		##2) Changing tags
+		newRepo = "docker-registry.local:30750/bioconda/"+tool_name
 		try:
-			if crt_image.tag (repository = "containers.biocontainers.pro/bioconda/"+tool_name,
+			if crt_image.tag (repository = newRepo,
 				tag = crt_tag):
 				print ("\tImage retagged")
 			else:
@@ -48,13 +52,41 @@ def pull_retag_and_push_images (client, tool_name, tags):
 		print (crt_image.tags) ###<- I've interrupted the script and seen that the new tag was indeed created but this doesn't return it...
 
 		##3) Pushing images to new registry
-		#TODO
+		try:
+			for line in client.images.push(newRepo, tag=crt_tag, stream=True, decode=True):
+				print (line)
+		except docker.errors.APIError:
+			print ("\tCould not push image "+new_repo+":"+crt_tag)
 
 	##4) Removing all images, another loop since removing each tag may waste time when different versions share layers
 	for crt_image in images:
 		print ("Removing image")
 		client.images.remove(crt_image.id, force=True)
 	return True
+
+def update_tool_tags_list (url, list_of_tags):
+	if list_of_tags == None:
+		return None
+	print ("\tTags in "+url)
+
+	##Making sure it doesn't exist in a docker registry
+	try:
+		 r2 = requests.get(url)
+	except requests.exceptions.RequestException:
+		print ("Error while checking tags availability")
+		return list_of_tags
+
+	if r2.status_code !=200:
+		print (None)
+		return list_of_tags
+	else:
+		cont_tags = r2.json()['tags']
+		print (cont_tags)
+		diff_of_images = list(set(list_of_tags) - set(cont_tags))
+		if len(diff_of_images) < 1:
+			return None
+		else:
+			return diff_of_images
 
 ###################################################
 ####################Main method####################
@@ -83,7 +115,7 @@ with open(args.listoftools, 'r') as listfile:
 		crt_line = crt_line.rstrip().split("\t")
 		crt_tool = crt_line[0]
 		crt_version = crt_line[1]
-		print (crt_tool, file = output)
+		print ("TREATING TOOL:"+crt_tool, file = output)
 
 		##Getting/checking bioconda's image availability
 		params = {'onlyActiveTags': 'true', 'limit': '100'}
@@ -100,28 +132,18 @@ with open(args.listoftools, 'r') as listfile:
 		print (crt_tags)
 		images = None
 
-		##Making sure it doesn't exist in our docker registry
+		##Checking that tool isn't already in biocontainers
 		url = "https://containers.biocontainers.pro/v2/biocontainers/"+crt_tool+"/tags/list"
-		r2 = requests.get(url)
-		if r2.status_code !=200:
-			print ("\tNot found in biocontainers, copy and push everything", file=output)
-			#Means we have to copy all images
-			pull_retag_and_push_images (client, crt_tool, crt_tags)
-		else:
-			print ("\tAlready exists in biocontainers registry:", file=output)
-			cont_tags = r2.json()['tags']
-			print (cont_tags)
-			#print (r2.headers)
-			#Means we must only push the images whose tags differ
-			diff_of_images = list(set(crt_tags) - set(cont_tags))
-			if len(diff_of_images) < 1:
-				print ("\tNothing to do here")
-			else:
-				print ("\tOnly copy and push the difference:")
-				print (diff_of_images)
-				pull_retag_and_push_images (client, crt_tool, diff_of_images)
+		crt_tags = update_tool_tags_list(url, crt_tags)
 
+		##Checking that tool isn't already imported in biocontainers 'bioconda' subrepo
+		url2 = "https://containers.biocontainers.pro/v2/bioconda/"+crt_tool+"/tags/list"
+		crt_tags = update_tool_tags_list(url2, crt_tags)
 
+		print ("After checking redundancy, here is the list of tags that need to be imported:")
+		print (crt_tags)
+
+		pull_retag_and_push_images (client, crt_tool, crt_tags)
 		##For testing:
 		if testCtr > 5:
 			sys.exit()
