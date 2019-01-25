5a49ec35d47340963c2e54929862d96c71b2eefc
raynooo <renaud.jullien@gmail.com>
2019-01-25 16:28:53 +0000

    Replace prints by log to allow level display tuning

--- bioconda_checkup/copy_conda_images_to_our_registry.py
+++ bioconda_checkup/copy_conda_images_to_our_registry.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import logging
 import argparse
 import requests
 import os
@@ -16,14 +17,14 @@ from collections import defaultdict
 def get_tags_from_response (response):
 	content = response.json()
 	res = list()
-	#print (content['tags'])
 	for tag in content['tags']:
 		res.append(tag['name'])
 	return res
 
-def pull_retag_and_push_images (client, tool_name, tags, output):
+#def pull_retag_and_push_images (client, tool_name, tags, output):
+def pull_retag_and_push_images (client, tool_name, tags):
 	if tags==None:
-		print ("Nothing to do here", file=output)
+		logging.info("Nothing to do here")
 		return False
 	images = list()
 
@@ -31,58 +32,57 @@ def pull_retag_and_push_images (client, tool_name, tags, output):
 	for crt_tag in tags:
 		try:
 			img_repo = "quay.io/biocontainers/"+tool_name
-			print ("Pulling "+img_repo+":"+crt_tag, file=output)
+			logging.info ("Pulling "+img_repo+":"+crt_tag)
 			crt_image = client.images.pull(img_repo, tag = crt_tag)
 			images.append(crt_image)
 		except docker.errors.APIError:
-			print ("\tCould not pull image "+tool_name+":"+crt_tag, file=output)
+			logging.warning ("\tCould not pull image "+tool_name+":"+crt_tag)
 			continue
-			#return False
 
 		##2) Changing tags
 		newRepo = "docker-registry.local:30750/bioconda/"+tool_name
 		try:
 			if crt_image.tag (repository = newRepo,
 				tag = crt_tag):
-				print ("\tImage retagged", file=output)
+				logging.info ("\tImage retagged")
 			else:
-				print ("\tImage NOT retagged", file=output)
+				logging.warning ("\tImage "+tool_name+":"+crt_tag+" NOT retagged")
 		except docker.errors.APIError:
-			print ("\tCould not retag image "+tool_name+":"+crt_tag, file=output)
-		print (crt_image.tags, file=output) ###<- I've interrupted the script and seen that the new tag was indeed created but this doesn't return it...
+			logging.warning("\tError while retagging "+tool_name+":"+crt_tag)
 
 		##3) Pushing images to new registry
 		try:
 			for line in client.images.push(newRepo, tag=crt_tag, stream=True, decode=True):
-				print (line) ##Not printing to log as it displays too many lines
-			print ("\tImage pushed", file=output)
+				logging.debug (line) ##Only for debug as it floods logs otherwise
+			logging.info ("\tImage pushed")
 		except docker.errors.APIError:
-			print ("\tCould not push image "+new_repo+":"+crt_tag, file=output)
+			logging.warning("\tCould not push image "+new_repo+":"+crt_tag)
 
 	##4) Removing all images, another loop since removing each tag may waste time when different versions share layers
 	for crt_image in images:
-		print ("Removing image", file=output)
+		logging.info ("Removing image")
 		client.images.remove(crt_image.id, force=True)
 	return True
 
-def update_tool_tags_list (url, list_of_tags, output):
+#def update_tool_tags_list (url, list_of_tags, output):
+def update_tool_tags_list (url, list_of_tags):
 	if list_of_tags == None:
 		return None
-	print ("\tTags in "+url, file=output)
+	logging.info("\tTags in "+url)
 
 	##Making sure it doesn't exist in a docker registry
 	try:
 		 r2 = requests.get(url)
 	except requests.exceptions.RequestException:
-		print ("Error while checking tags availability", file=output)
+		logging.warning("Error while checking tags availability at "+url)
 		return list_of_tags
 
 	if r2.status_code !=200:
-		print (None, file=output)
+		logging.info(None)
 		return list_of_tags
 	else:
 		cont_tags = r2.json()['tags']
-		print (cont_tags, file=output)
+		logging.info (cont_tags)
 		diff_of_images = list(set(list_of_tags) - set(cont_tags))
 		if len(diff_of_images) < 1:
 			return None
@@ -94,21 +94,32 @@ def update_tool_tags_list (url, list_of_tags, output):
 ###################################################
 
 parser = argparse.ArgumentParser(description="Builds BioConda tools from local copy of repo based on list and pushes the images")
-parser.add_argument('-l','--listoftools', help="Path to a file containing the list of tools needing to be built and pushed", required="true")
-parser.add_argument('-o','--output', help="Where the logs are printed, defaults to stdout")
+parser.add_argument('-i','--inputlist', help="Path to a file containing the list of tools needing to be built and pushed", required="true")
+parser.add_argument('--debug', help="Displays debug level log", action="store_true")
+parser.add_argument('-l','--logfile', help="Path to log file")
 args = parser.parse_args()
 
-if args.output is not None:
-        output = open (args.output, "w")
+testCtr = 1
+
+if args.debug:
+	level = logging.DEBUG
 else:
-        output = sys.stdout
+	level = logging.INFO
 
-totalSize = 0
-testCtr = 0
+if args.logfile:
+	logging.basicConfig(
+		level=level,
+		format="%(levelname)s:%(message)s",
+		filename=args.logfile
+	)
+else:
+	logging.basicConfig(
+		level=level,
+		format="%(levelname)s:%(message)s"
+		)
 
-with open(args.listoftools, 'r') as listfile:
+with open(args.inputlist, 'r') as listfile:
 	tools = listfile.readlines()
-	#tools = sorted(tools)
 	client = docker.from_env()
 
 	for crt_line in tools:
@@ -116,7 +127,7 @@ with open(args.listoftools, 'r') as listfile:
 		crt_line = crt_line.rstrip().split("\t")
 		crt_tool = crt_line[0]
 		crt_version = crt_line[1]
-		print ("TREATING TOOL:"+crt_tool+" (number "+str(testCtr)+")", file = output)
+		logging.info ("TREATING TOOL:"+crt_tool+" (number "+str(testCtr)+")")
 
 		##Getting/checking bioconda's image availability
 		params = {'onlyActiveTags': 'true', 'limit': '100'}
@@ -125,35 +136,27 @@ with open(args.listoftools, 'r') as listfile:
 		if r.status_code != 200:
 			continue
 		ret = r.json()
-		for tag in ret['tags']:
-			#print ("\t"+tag['name'], file=output)
-			totalSize += tag['size']
-			#print ("Current size: "+str(totalSize))
 		crt_tags = get_tags_from_response(r)
-		print (crt_tags, file=output)
+		logging.info(crt_tags)
 		images = None
 
 		##Checking that tool isn't already in biocontainers
 		url = "https://containers.biocontainers.pro/v2/biocontainers/"+crt_tool+"/tags/list"
-		crt_tags = update_tool_tags_list(url, crt_tags, output)
+		crt_tags = update_tool_tags_list(url, crt_tags)
+
 
 		##Checking that tool isn't already imported in biocontainers 'bioconda' subrepo
 		url2 = "https://containers.biocontainers.pro/v2/bioconda/"+crt_tool+"/tags/list"
-		crt_tags = update_tool_tags_list(url2, crt_tags, output)
+		crt_tags = update_tool_tags_list(url2, crt_tags)
 
-		print ("After checking redundancy, here is the list of tags that need to be imported:", file=output)
-		print (crt_tags, file=output)
 
-		pull_retag_and_push_images (client, crt_tool, crt_tags, output)
+		logging.info ("After checking redundancy, here is the list of tags that need to be imported:")
+		logging.info (crt_tags)
+
+		pull_retag_and_push_images (client, crt_tool, crt_tags)
+
 		##For testing:
-		if testCtr > 5:
+		if testCtr > 50:
 			sys.exit()
-
-print ("Total size required to store images: "+str(totalSize), file = output)
-
-if output is not sys.stdout:
-        output.close()
-
-#print ("Found "+str(ctr)+" tools in common")
 
 
